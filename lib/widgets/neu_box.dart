30668c7703e6ff9f86f29e92df7e70182310a153
Med <geek.4tech007@gmail.com>
2024-10-21 10:25:19 +0000

    Fixed neumorphic look for dark mode 💄

--- lib/widgets/neu_box.dart
+++ lib/widgets/neu_box.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mini_music/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class NeuBox extends StatelessWidget {
   final Widget? child;
@@ -6,20 +8,21 @@ class NeuBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isDarkMode = Provider.of<ThemeProvider>(context).isDarkMode;
     return Container(
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.surface,
         borderRadius: BorderRadius.circular(16),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.shade500,
+            color: isDarkMode ? Colors.black : Colors.grey.shade500,
             blurRadius: 16,
             offset: const Offset(4, 4),
           ),
-          const BoxShadow(
-            color: Colors.white,
+          BoxShadow(
+            color: isDarkMode ? Colors.grey.shade800 : Colors.white,
             blurRadius: 16,
-            offset: Offset(-4, -4),
+            offset: const Offset(-4, -4),
           )
         ],
       ),
